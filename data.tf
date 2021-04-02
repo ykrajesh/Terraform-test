0c6f007bc21b49e8bd6d09d3c55de0ae88896227
Rajesh Yadav <ykrajesh@outlook.com>
2021-04-02 21:13:11 +0530

    subscription change

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "rg" {
-  name = "learn-5213847c-a9af-4e83-847f-d9710a97e883"
+  name = "learn-bf57c589-6099-44d9-834d-da9eab49f509"
 }
 
 data "azurerm_virtual_network" "vnetinfo" {
